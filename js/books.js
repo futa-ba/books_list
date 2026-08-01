5e5547ec81917a68cf5322fdf50773d878780e71
futaba <futabayamauchi608@gmail.com>
2026-08-01 23:36:25 +0900

    Refactor loadBooks function to use script tag

--- js/books.js
+++ js/books.js
@@ -2,40 +2,37 @@ const API =
 "https://script.google.com/macros/s/AKfycbwWThRcouiyNIGwM_RJ97uP8KUDn_4unTzmjcKW9VXeIyep4nyZJ2PelIlRPxauP-AE0Q/exec";
 
 
-async function loadBooks(){
+function loadBooks(){
+
+    const script = document.createElement("script");
+
+    script.src = API + "?callback=displayBooks";
+
+    document.body.appendChild(script);
+
+}
 
 
-    const response = await fetch(API);
-
-
-    const books = await response.json();
-
+function displayBooks(books){
 
 
     const tbody =
         document.querySelector("#bookTable tbody");
 
 
-
     tbody.innerHTML = "";
 
 
-
-    books.forEach(book => {
+    books.forEach(book=>{
 
 
         tbody.innerHTML += `
 
         <tr>
-
             <td>${book.ID}</td>
-
             <td>${book.タイトル}</td>
-
             <td>${book.著者}</td>
-
             <td>${book.状態}</td>
-
         </tr>
 
         `;
