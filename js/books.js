b713c7f2a0700ced7cd4a39cf1cfc776f7985fb3
futaba <futabayamauchi608@gmail.com>
2026-08-02 13:56:53 +0900

    Update API endpoint in books.js

--- js/books.js
+++ js/books.js
@@ -1,7 +1,7 @@
 console.log("books.js start");
 
 const API =
-"https://script.google.com/macros/s/AKfycbzXexSuZm4Ao2g3MfQe_34lLhpozxZ51QSueNFKesYE1xfd0K0695IEm3tB6IeTJudBTQ/exec";
+"https://script.google.com/macros/s/AKfycbzz5_U5QHJ-HaW74IJWB4H_t4KHTSaZFPBmiILsXCGp7bgahdJQtBPpYz10E_2ERePPEA/exec";
 
 
 let allBooks = [];
