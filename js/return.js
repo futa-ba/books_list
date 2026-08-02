0e31a807b4c881aba1e56047c3d7834a2e431be8
futaba <futabayamauchi608@gmail.com>
2026-08-02 20:06:06 +0900

    Update return.js

--- js/return.js
+++ js/return.js
@@ -2,262 +2,198 @@ const API =
 "https://script.google.com/macros/s/AKfycbxcV3V1dVrwK21Fv4w57dwrofXCKm0vCWcuOvnjNo9781DWbX2hyb_Hw_Ao6-wWmWX2Vg/exec";
 
 
+
 async function searchBooks(){
 
 
-const email =
-document
-.getElementById("email")
-.value
-.trim();
+    const emailElement =
+    document.getElementById("email");
 
 
 
-if(email === ""){
+    if(!emailElement){
 
-    alert("メールアドレスを入力してください");
+        alert("メール入力欄がありません");
 
-    return;
+        return;
+
+    }
+
+
+
+    const email =
+    emailElement.value.trim();
+
+
+
+    if(email === ""){
+
+        alert("メールアドレスを入力してください");
+
+        return;
+
+    }
+
+
+
+
+    const response =
+    await fetch(
+
+        API
+        +
+        "?action=returnSearch&email="
+        +
+        encodeURIComponent(email)
+
+    );
+
+
+
+
+    const books =
+    await response.json();
+
+
+
+
+    const tbody =
+    document.getElementById("returnList");
+
+
+
+    tbody.innerHTML = "";
+
+
+
+
+    if(books.length === 0){
+
+
+        tbody.innerHTML = `
+
+        <tr>
+
+        <td colspan="4">
+
+        貸出中の本がありません
+
+        </td>
+
+        </tr>
+
+        `;
+
+
+        return;
+
+
+    }
+
+
+
+
+
+    books.forEach(book=>{
+
+
+        tbody.innerHTML += `
+
+
+        <tr>
+
+
+        <td data-label="図書ID">
+
+        ${book[3]}
+
+        </td>
+
+
+
+        <td data-label="タイトル">
+
+        ${book[4]}
+
+        </td>
+
+
+
+        <td data-label="状態">
+
+        ${book[5]}
+
+        </td>
+
+
+
+        <td data-label="返却">
+
+
+        <button onclick="returnBook('${book[3]}')">
+
+        返却
+
+        </button>
+
+
+        </td>
+
+
+
+        </tr>
+
+
+        `;
+
+
+    });
+
+
 
 }
 
 
-
-console.log("検索メール:", email);
-
-
-
-const response =
-await fetch(
-
-API
-+
-"?action=returnSearch&email="
-+
-encodeURIComponent(email)
-
-);
-
-
-
-const books =
-await response.json();
-
-
-
-console.log("検索結果:", books);
-
-
-
-const tbody =
-document
-.getElementById("returnList");
-
-
-
-tbody.innerHTML="";
-
-
-
-if(books.length === 0){
-
-tbody.innerHTML = `
-
-<tr>
-
-<td colspan="4">
-
-貸出中の本がありません
-
-</td>
-
-</tr>
-
-`;
-
-return;
-
-}
-
-
-
-books.forEach(book=>{
-
-
-tbody.innerHTML += `
-
-<tr>
-
-
-<td data-label="図書ID">
-
-${book[3]}
-
-</td>
-
-
-
-<td data-label="タイトル">
-
-${book[4]}
-
-</td>
-
-
-
-<td data-label="状態">
-
-${book[5]}
-
-</td>
-
-
-
-<td data-label="返却">
-
-
-<button onclick="returnBook('${book[3]}')">
-
-返却
-
-</button>
-
-
-</td>
-
-
-</tr>
-
-
-`;
-
-});
-
-
-}
-
-
-
-const response =
-await fetch(
-
-API
-+
-"?action=returnSearch&email="
-+
-encodeURIComponent(email)
-
-);
-
-
-
-const books =
-await response.json();
-
-
-
-const tbody =
-document
-.getElementById("returnList");
-
-
-
-tbody.innerHTML="";
-
-
-
-books.forEach(book=>{
-
-
-tbody.innerHTML += `
-
-<tr>
-
-
-<td data-label="図書ID">
-
-${book[3]}
-
-</td>
-
-
-
-<td data-label="タイトル">
-
-${book[4]}
-
-</td>
-
-
-
-<td data-label="状態">
-
-<span class="status borrowed">
-
-${book[5]}
-
-</span>
-
-</td>
-
-
-
-<td data-label="返却">
-
-
-<button
-onclick="returnBook('${book[3]}')">
-
-返却
-
-</button>
-
-
-</td>
-
-
-</tr>
-
-`;
-
-
-
-});
-
-
-});
-
-
-}
 
 
 
 async function returnBook(bookId){
 
 
-const response =
-await fetch(
 
-API
-+
-"?action=returnBook&bookId="
-+
-encodeURIComponent(bookId)
-
-);
+    console.log(
+        "返却ID:",
+        bookId
+    );
 
 
 
-const result =
-await response.text();
+    const response =
+    await fetch(
+
+        API
+        +
+        "?action=returnBook&bookId="
+        +
+        encodeURIComponent(bookId)
+
+    );
 
 
 
-alert(result);
+
+    const result =
+    await response.text();
 
 
 
-location.reload();
+
+    alert(result);
+
+
+
+    location.reload();
+
 
 
 }
