574c1f1bfc9b8e6cfff7363ceb9216007706655e
futaba <futabayamauchi608@gmail.com>
2026-08-02 20:04:04 +0900

    Update return.js

--- js/return.js
+++ js/return.js
@@ -5,24 +5,136 @@ const API =
 async function searchBooks(){
 
 
-const emailElement =
-document.getElementById("email");
+const email =
+document
+.getElementById("email")
+.value
+.trim();
 
 
-console.log("email element:", emailElement);
 
+if(email === ""){
 
-if(!emailElement){
-
-    alert("メール入力欄が見つかりません");
+    alert("メールアドレスを入力してください");
 
     return;
 
 }
 
 
-const email =
-emailElement.value;
+
+console.log("検索メール:", email);
+
+
+
+const response =
+await fetch(
+
+API
++
+"?action=returnSearch&email="
++
+encodeURIComponent(email)
+
+);
+
+
+
+const books =
+await response.json();
+
+
+
+console.log("検索結果:", books);
+
+
+
+const tbody =
+document
+.getElementById("returnList");
+
+
+
+tbody.innerHTML="";
+
+
+
+if(books.length === 0){
+
+tbody.innerHTML = `
+
+<tr>
+
+<td colspan="4">
+
+貸出中の本がありません
+
+</td>
+
+</tr>
+
+`;
+
+return;
+
+}
+
+
+
+books.forEach(book=>{
+
+
+tbody.innerHTML += `
+
+<tr>
+
+
+<td data-label="図書ID">
+
+${book[3]}
+
+</td>
+
+
+
+<td data-label="タイトル">
+
+${book[4]}
+
+</td>
+
+
+
+<td data-label="状態">
+
+${book[5]}
+
+</td>
+
+
+
+<td data-label="返却">
+
+
+<button onclick="returnBook('${book[3]}')">
+
+返却
+
+</button>
+
+
+</td>
+
+
+</tr>
+
+
+`;
+
+});
+
+
+}
 
 
 
